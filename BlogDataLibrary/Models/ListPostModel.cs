aabfee6010a6499364c93c5c243b3bc4f4956575
jsamp-it <sampoleojosecarlos@gmail.com>
2025-08-28 19:41:03 +0800

    Delete BlogDataLibrary directory

--- BlogDataLibrary/Models/ListPostModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlogDataLibrary.Models
-{
-    public class ListPostModel
-    {
-        public string Title { get; set; }
-        public string Username { get; set; }
-        public DateTime CreatedDate { get; set; }
-    }
-}
