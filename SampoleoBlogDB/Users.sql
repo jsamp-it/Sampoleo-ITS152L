29b308d555a08a73a952987db2fe093314b4ba20
jsamp-it <sampoleojosecarlos@gmail.com>
2025-08-28 19:41:20 +0800

    Delete SampoleoBlogDB directory

--- SampoleoBlogDB/Users.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Users] (
-    [Id] INT IDENTITY (1, 1) NOT NULL,
-    [Username] NVARCHAR(50) NOT NULL,
-    [Password] NVARCHAR(50) NOT NULL,
-    [FirstName] NVARCHAR(50) NOT NULL,
-    [LastName] NVARCHAR(50) NOT NULL,
-    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
